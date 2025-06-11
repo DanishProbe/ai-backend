cf9ea5aa86292aa601b3849138f596a5315817b9
DanishProbe <lohse@danishprobe.com>
2025-06-11 22:54:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,7 @@ def manage_laws():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port, debug=True)
+
