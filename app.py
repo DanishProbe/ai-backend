411765c2f91b44d3b1cc5df8dd413185a4c034a2
DanishProbe <lohse@danishprobe.com>
2025-06-12 15:11:02 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -19,11 +19,9 @@ class Law(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(500))
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @app.route('/rules', methods=['GET', 'POST', 'DELETE'])
 def manage_rules():
