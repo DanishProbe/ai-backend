c0f4cd229aaef0ce39b326ac4541321d1bf0f11e
DanishProbe <lohse@danishprobe.com>
2025-06-26 14:21:17 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-# app.py with app.app_context() fix in background thread
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def healthcheck():
+    return "Backend kører OK"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000, debug=True)
